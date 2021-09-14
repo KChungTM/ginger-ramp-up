536c88da16dfb0dcd5a02d28969330138b928740
Surbhi Jain <surbhij@berkeley.edu>
2021-09-14 16:34:54 -0700

    compelted ecommerce tutorial

---
+++ ecommerce/ecommerce/products/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Category, Book, Grocery
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(Grocery)
